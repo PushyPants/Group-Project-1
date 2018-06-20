d5e0ff1639f4d5753c87d59b10a0c23faa8e771f
RMSanchez <raulsg93@gmail.com>
2018-06-19 21:10:03 -0500

    got modal to open on page loading, along with a functional form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 
+    //this is pertaining to the modal on the splash page <plz do not delete my dudes>
+
+    $(window).on('load',function(){
+        $('#myModal').modal('show');
+    });
+
+    //this plays my song--- or should
+    var song = document.createElement('audio');
+    song.setAttribute("src", ".//audio/travel-song");
+    song.play();
+
+
     
 });
