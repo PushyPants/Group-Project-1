2621a684a7347a8774494f8dd9e6f440502bb28c
Push <crashjohnson@gmail.com>
2018-06-19 23:44:03 -0500

    Although not working, added ajax request to pull info from eventful, getting a cross-origin error

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
 
     console.log(bandName);
     
+    //call eventful API and place the band name in the seach paramater of the endpoint
+
+    //NOT WORKING CROSS ORIGIN ERROR
+
+    $.ajax({
+        url: `http://api.eventful.com/json/performers/search?app_key=dT9kBLwTGpSRrDZQ&keywords=`+bandName,
+        method: 'GET',
+        dataSet: 'jsonp',
+    }).then(function(response){
+        console.log(response);
+    })
+
+
+
+
+
+
+
+
+
 });
