be7681f8aeb75905bd0092dc4cb8c605caf9d306
Push <crashjohnson@gmail.com>
2018-06-20 18:26:58 -0500

    Ajax Response working

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,20 @@ $(document).ready(function(){
     bandName = autoComplete.split(' ').join('+').toLowerCase();
 
     console.log(bandName);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 10
+      });
+
+    let evfKey = 'dT9kBLwTGpSRrDZQ';
+
+    $.ajax({
+        url: 'http://api.eventful.com/json/performers/events/list?app_key=dT9kBLwTGpSRrDZQ&id=P0-001-000000265-4',
+        method: 'GET',
+        dataType: 'jsonp',
+    }).then(function(response){
+        console.log(response);
+    })
     
 });
