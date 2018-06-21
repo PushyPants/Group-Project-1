45901c62dd9db0de1dc851f65421beee8befd7cd
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-06-20 20:17:43 -0500

    Moved map scripting from sandbox to working files

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,46 @@ $(document).ready(function(){
 
 
 
+    var start ={
+        lat: 29.743414,
+        lng: -95.392648
+    };
 
+    var end = {
+        lat:30.266474,
+        lng: -97.740786
+    };
+    function initMap() {
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: start
+        });
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        //var selectedMode = "DRIVING";
+        directionsService.route({
+          origin: start,  
+          destination: end,  
+          // Note that Javascript allows us to access the constant
+          // using square brackets and a string value as its
+          // "property."
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status == 'OK') {
+            directionsDisplay.setDirections(response);
+            console.log(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+    }
 
 
 
